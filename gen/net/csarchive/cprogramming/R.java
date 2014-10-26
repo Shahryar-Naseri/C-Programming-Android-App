1a662a1910162b7bbe70263f99662f717020b0fc
Shahryar Naseri <shahryar@csarchive.net>
2014-10-27 04:15:43 +0530

    Created all XML files for the application user interface

--- gen/net/csarchive/cprogramming/R.java
+++ gen/net/csarchive/cprogramming/R.java
@@ -1649,11 +1649,22 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f090030;
         public static final int action_mode_bar_stub=0x7f09002f;
         public static final int action_mode_close_button=0x7f090022;
-        public static final int action_settings=0x7f090041;
+        public static final int action_settings=0x7f09005c;
         public static final int activity_chooser_view_content=0x7f090023;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
         public static final int bottom=0x7f09001e;
+        public static final int btnAbout=0x7f090058;
+        public static final int btnContact=0x7f090057;
+        public static final int btnCopy=0x7f090051;
+        public static final int btnDecrease=0x7f09004f;
+        public static final int btnFontColor=0x7f090052;
+        public static final int btnIncrease=0x7f09004d;
+        public static final int btnProgramsList=0x7f090054;
+        public static final int btnSaveAll=0x7f090056;
+        public static final int btnSend=0x7f09004c;
+        public static final int btnShare=0x7f09004e;
+        public static final int btnViewAll=0x7f090055;
         public static final int checkbox=0x7f09002b;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f090031;
@@ -1663,6 +1674,9 @@ containing a value of this type.
         public static final int dropdown=0x7f09001a;
         public static final int edit_query=0x7f090035;
         public static final int end=0x7f09001c;
+        public static final int etMessage=0x7f09004b;
+        public static final int etName=0x7f090047;
+        public static final int etSubject=0x7f090049;
         public static final int expand_activities_button=0x7f090024;
         public static final int expanded_menu=0x7f09002a;
         public static final int home=0x7f090000;
@@ -1698,6 +1712,19 @@ containing a value of this type.
         public static final int tabMode=0x7f09000c;
         public static final int title=0x7f090029;
         public static final int top=0x7f09001f;
+        public static final int tvCreate=0x7f090042;
+        public static final int tvDisplay=0x7f090050;
+        public static final int tvEmail=0x7f090045;
+        public static final int tvInfo=0x7f090043;
+        public static final int tvIntro=0x7f090044;
+        public static final int tvList=0x7f090053;
+        public static final int tvMessage=0x7f09004a;
+        public static final int tvName=0x7f090046;
+        public static final int tvSave=0x7f090059;
+        public static final int tvSplashDesc=0x7f09005b;
+        public static final int tvSplashTitle=0x7f09005a;
+        public static final int tvSubject=0x7f090048;
+        public static final int tvVersion=0x7f090041;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
         public static final int withText=0x7f090018;
@@ -1752,8 +1779,14 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030014;
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
-        public static final int main_menu=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int about=0x7f030017;
+        public static final int contact=0x7f030018;
+        public static final int display=0x7f030019;
+        public static final int list=0x7f03001a;
+        public static final int main_menu=0x7f03001b;
+        public static final int save=0x7f03001c;
+        public static final int splash=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
         public static final int main_menu=0x7f0c0000;
@@ -1814,7 +1847,30 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000d;
         public static final int action_settings=0x7f0a0011;
         public static final int app_name=0x7f0a000f;
+        public static final int btnAbout=0x7f0a0016;
+        public static final int btnContact=0x7f0a0015;
+        public static final int btnCopy=0x7f0a001c;
+        public static final int btnDecrease=0x7f0a001b;
+        public static final int btnFontColor=0x7f0a001d;
+        public static final int btnIncrease=0x7f0a0019;
+        public static final int btnProgramsList=0x7f0a0012;
+        public static final int btnSaveAll=0x7f0a0014;
+        public static final int btnSend=0x7f0a0026;
+        public static final int btnShare=0x7f0a001a;
+        public static final int btnViewAll=0x7f0a0013;
         public static final int hello_world=0x7f0a0010;
+        public static final int tvCreate=0x7f0a001f;
+        public static final int tvEmail=0x7f0a0022;
+        public static final int tvInfo=0x7f0a0020;
+        public static final int tvIntro=0x7f0a0021;
+        public static final int tvList=0x7f0a0027;
+        public static final int tvMessage=0x7f0a0025;
+        public static final int tvName=0x7f0a0023;
+        public static final int tvSave=0x7f0a0028;
+        public static final int tvSplashDesc=0x7f0a0018;
+        public static final int tvSplashTitle=0x7f0a0017;
+        public static final int tvSubject=0x7f0a0024;
+        public static final int tvVersion=0x7f0a001e;
     }
     public static final class style {
         /** 
