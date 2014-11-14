6c5ddb2c68c17545df44082514cbfe34839cdbd8
Shahryar Naseri <shahryar@csarchive.net>
2014-11-14 15:37:21 +0530

    Changed the WebView font size and improved the loading time

--- src/net/csarchive/cprogramming/About.java
+++ src/net/csarchive/cprogramming/About.java
@@ -2,12 +2,15 @@ package net.csarchive.cprogramming;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.RenderPriority;
 import android.webkit.WebView;
 
 public class About extends Activity{
 	
 	WebView wv;
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -15,6 +18,16 @@ public class About extends Activity{
 		String text = "<html><body style=\"text-align:justify;background:#198CFF;color:#ffffff;\"> %s </body></Html>";
 		String data = getString(R.string.tvCreate);
 		wv = (WebView) findViewById(R.id.wvInfo);
+		WebSettings websettings = wv.getSettings();
+		websettings.setDefaultFontSize(18);
+		websettings.setRenderPriority(RenderPriority.HIGH);
+		websettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+		websettings.setAppCacheEnabled(false);
+		websettings.setBlockNetworkImage(true);
+		websettings.setLoadsImagesAutomatically(true);
+		websettings.setGeolocationEnabled(false);
+		websettings.setNeedInitialFocus(false);
+		websettings.setSaveFormData(false);
 		wv.loadData(String.format(text, data), "text/html", "utf-8");
 	} 
 }
